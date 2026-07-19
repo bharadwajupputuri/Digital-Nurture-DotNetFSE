b782ecbd4666dd18f0e91da694dfb842c2a85247
bharadwajupputuri <bharadwajupputuri39@gmail.com>
2026-07-19 13:27:27 +0530

    Add files via upload

---
+++ DotNetFSE/DEEPSKILLING/NUnit and Moq/1. Moq-Handson/CustomerCommTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+
+        private Mock<IMailSender> mockMailSender;
+
+        private CustomerCommLib.CustomerComm customerComm;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            mockMailSender = new Mock<IMailSender>();
+
+
+            mockMailSender
+                .Setup(x => x.SendMail(
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+                .Returns(true);
+
+
+            customerComm =
+                new CustomerCommLib.CustomerComm(
+                    mockMailSender.Object
+                );
+        }
+
+
+        [Test]
+        public void SendMailToCustomer_ReturnsTrue()
+        {
+            bool result =
+                customerComm.SendMailToCustomer();
+
+
+            Assert.IsTrue(result);
+        }
+
+    }
+}
