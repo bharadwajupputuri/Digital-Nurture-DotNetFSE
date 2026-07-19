4502d442dfd770536244f0a0d55064a27f6cdf4f
bharadwajupputuri <bharadwajupputuri39@gmail.com>
2026-07-19 23:28:44 +0530

    Add files via upload

---
+++ DotNetFSE/DEEPSKILLING/Microservices/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("profile")]
+        public IActionResult Profile()
+        {
+            return Ok("Welcome Admin! JWT Token is valid.");
+        }
+    }
+}
