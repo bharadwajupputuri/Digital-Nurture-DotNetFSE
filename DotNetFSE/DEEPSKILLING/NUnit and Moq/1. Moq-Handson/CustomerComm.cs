52e3bda32484dc39abeb9f58c0c455727fec5395
bharadwajupputuri <bharadwajupputuri39@gmail.com>
2026-07-19 13:24:55 +0530

    Add files via upload

---
+++ DotNetFSE/DEEPSKILLING/NUnit and Moq/1. Moq-Handson/CustomerComm.cs
@@ -0,0 +1,30 @@
+﻿namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+
+        private IMailSender _mailSender;
+
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+
+        public bool SendMailToCustomer()
+        {
+
+            string customerEmail = "cust123@abc.com";
+
+            string message = "Some Message";
+
+
+            _mailSender.SendMail(customerEmail, message);
+
+
+            return true;
+        }
+
+    }
+}
