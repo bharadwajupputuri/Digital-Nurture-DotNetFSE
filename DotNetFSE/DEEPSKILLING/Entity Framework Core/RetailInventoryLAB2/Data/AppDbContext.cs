4c88d30f2ca19353f80297ad5df3b4fb97cbdb3f
bharadwajupputuri <bharadwajupputuri39@gmail.com>
2026-07-27 13:27:25 +0530

    Add files via upload

---
+++ DotNetFSE/DEEPSKILLING/Entity Framework Core/RetailInventoryLAB2/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventoryLAB2.Models;
+
+namespace RetailInventoryLAB2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=RetailInventoryLab2Db;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ DotNetFSE/DEEPSKILLING/Entity Framework Core/RetailInventoryLAB2/Program.cs
@@ -0,0 +1,31 @@
+﻿using RetailInventoryLAB2.Data;
+using RetailInventoryLAB2.Models;
+
+using var context = new AppDbContext();
+
+// Create the database
+context.Database.EnsureCreated();
+
+// Add Category
+var category = new Category
+{
+    Name = "Electronics"
+};
+
+context.Categories.Add(category);
+context.SaveChanges();
+
+// Add Product
+var product = new Product
+{
+    Name = "Laptop",
+    Price = 55000,
+    CategoryId = category.Id
+};
+
+context.Products.Add(product);
+context.SaveChanges();
+
+Console.WriteLine("Category inserted successfully.");
+Console.WriteLine("Product inserted successfully.");
+Console.WriteLine("Database created successfully.");
