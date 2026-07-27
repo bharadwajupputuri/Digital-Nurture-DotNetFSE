2d7528ceedefccd4139bf6b950af801f9494b605
bharadwajupputuri <bharadwajupputuri39@gmail.com>
2026-07-27 19:40:26 +0530

    Add files via upload

---
+++ DotNetFSE/DEEPSKILLING/Entity Framework Core/RetailInventoryLAB4/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventoryLAB4.Models;
+
+namespace RetailInventoryLAB4.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server=(localdb)\\MSSQLLocalDB;Database=RetailInventoryLab4Db;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+    }
+}
