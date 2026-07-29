1edd1fdc067386bed0451ba23dc3a5aae9ca352b
bharadwajupputuri <bharadwajupputuri39@gmail.com>
2026-07-29 11:29:37 +0530

    Add files via upload

---
+++ DotNetFSE/DEEPSKILLING/React/9.ReactJS_HOL/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ListofPlayers from "./ListofPlayers";
+import IndianPlayers from "./IndianPlayers";
+
+function App() {
+
+    const flag = false;
+
+    if(flag){
+
+        return <ListofPlayers/>;
+
+    }
+
+    else{
+
+        return <IndianPlayers/>;
+
+    }
+
+}
+
+export default App;
